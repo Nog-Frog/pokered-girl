3e9f3d7d97fa11960be51c61d0f889ffa1192106
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-23 19:06:26 +0300

    Update CeladonMansionRoofHouse.asm

--- text/maps/CeladonMansionRoofHouse.asm
+++ text/maps/CeladonMansionRoofHouse.asm
@@ -1,10 +1,8 @@
 _CeladonMansion5Text1::
-	text "I know everything"
-	line "about the world"
-	cont "of #MON in"
-	cont "your GAME BOY!"
+	text "אני יודע הכל"
+	line "על עולם ה#ימונים"
+	cont "שבGAME BOY שלך!" ;TODO
 
-	para "Get together with"
-	line "your friends and"
-	cont "trade #MON!"
+	para "היפגש עם חברייך"
+	line "והחלף איתם #ימונים!"
 	done
