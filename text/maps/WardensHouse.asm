9c22a63de140099550855b5d3e5e401e0eeb21dd
NoamGeller <noam.geller@mail.huji.ac.il>
2020-06-09 19:37:30 +0300

    Translated WardenHouse

--- text/maps/WardensHouse.asm
+++ text/maps/WardensHouse.asm
@@ -1,84 +1,81 @@
 _WardenGibberishText1::
-	text "WARDEN: Hif fuff"
-	line "hefifoo!"
+	text "WARDEN: פה הופא!" ; TODO
 
-	para "Ha lof ha feef ee"
-	line "hafahi ho. Heff"
-	cont "hee fwee!"
+	para "איפהי אה הפיאיי"
+	line "פלי פהאפאהי. עפור"
+	cont "לי פפקפה!"
 	done
 
 _WardenGibberishText2::
-	text "Ah howhee ho hoo!"
-	line "Eef ee hafahi ho!"
+	text "אהי פופך עאיך!"
+	line "פה פהאפאהי!"
 	done
 
 _WardenGibberishText3::
-	text "Ha? He ohay heh"
-	line "ha hoo ee haheh!"
+	text "הא? פפהר, אפל אהי"
+	line "מחפף אפעפור לי!"
 	done
 
 _WardenTeethText1::
-	text "<PLAYER> gave the"
-	line "GOLD TEETH to the"
-	cont "WARDEN!@@"
+	text "<PLAYER> נתן את"
+	line "ה-GOLD TEETH" ; TODO
+	cont "ל-WARDEN!@@" ; TODO
 
 _WardenTeethText2::
 	text ""
 
-	para "The WARDEN popped"
-	line "in his teeth!"
+	para "ה-WARDEN הכניס" ; TODO
+	line "את שיניו פנימה!"
 	prompt
 
 _WardenThankYouText::
-	text "WARDEN: Thanks,"
-	line "kid! No one could"
-	cont "understand a word"
-	cont "that I said."
+	text "WARDEN: תודה," ; TODO
+	line "ילד! אף אחד לא"
+	cont "הבין מילה שאמרתי."
 
-	para "I couldn't work"
-	line "that way."
-	cont "Let me give you"
-	cont "something for"
-	cont "your trouble."
+	para "לא יכולתי לעבוד"
+	line "ככה. הרשה לי"
+	cont "להעניק לך משהו"
+	cont "עבור הטרחה."
 	prompt
 
 _ReceivedHM04Text::
-	text "<PLAYER> received"
+	text "<PLAYER> קיבל"
 	line "@"
 	TX_RAM wcf4b
 	text "!@@"
 
 _HM04ExplanationText::
-	text "WARDEN: HM04"
-	line "teaches STRENGTH!"
+	text "WARDEN: HM04" ; TODO
+	line "מלמד STRENGTH!" ; TODO
 
-	para "It lets #MON"
-	line "move boulders"
-	cont "when you're out-"
-	cont "side of battle."
+	para "זה מאפשר ל#ימון"
+	line "להזיז סלעים"
+	cont "כאשר אתה"
+	cont "מחוץ לקרב."
 
-	para "Oh yes, did you"
-	line "find SECRET HOUSE"
-	cont "in SAFARI ZONE?"
+	para "אה כן, מצאת את"
+	line "SECRET HOUSE" ; TODO
+	cont "ב-SAFARI ZONE?" ; TODO
 
-	para "If you do, you"
-	line "win an HM!"
+	para "אם אתה מוצא, אתה"
+	line "זוכה ב-HM!"
 
-	para "I hear it's the"
-	line "rare SURF HM."
+	para "השמועה אומרת שזה"
+	line "ףSURF HM הנדיר." ; TODO
 	done
 
 _HM04NoRoomText::
-	text "Your pack is"
-	line "stuffed full!"
+	text "התרמיל שלך"
+	line "מלא לגמרי!"
 	done
 
 _FuchsiaHouse2Text_75176::
-	text "#MON photos"
-	line "and fossils."
+	text "תמונות של #ימון"
+	line "ומאובנים."
 	done
 
 _FuchsiaHouse2Text_7517b::
-	text "Old #MON"
-	line "merchandise."
+	text "סחורת #ימון"
+	line "ישנה."
 	done
