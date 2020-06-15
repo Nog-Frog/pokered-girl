6050ea7c46d3594915b0f7ca1f7758d59cf4b623
Shako-24 <66572118+Shako-24@users.noreply.github.com>
2020-06-15 23:59:56 +0300

    Update FuchsiaCity.asm

--- text/maps/FuchsiaCity.asm
+++ text/maps/FuchsiaCity.asm
@@ -1,30 +1,31 @@
 _FuchsiaCityText1::
-	text "Did you try the"
-	line "SAFARI GAME? Some"
-	cont "#MON can only"
-	cont "be caught there."
+	text "ניסית את"
+	line "?SAFARI GAMEה";TODO
+	cont "רק שם תוכל"
+	cont ".לתפוס #ימונים"
 	done
 
 _FuchsiaCityText2::
-	text "SAFARI ZONE has a"
-	line "zoo in front of"
-	cont "the entrance."
+	text "יש גן חיות"
+	line "מול הכניסה"
+	cont "SAFARI ZONEל";TODO
 
-	para "Out back is the"
-	line "SAFARI GAME for"
-	cont "catching #MON."
+	para "בחוץ מאחורה"
+	line "SAFARI GAME נמצא";TODO
+	cont "כדי לתפוס"
+	cont "ימונים#"
 	done
 
 _FuchsiaCityText3::
-	text "ERIK: Where's"
-	line "SARA? I said I'd"
-	cont "meet her here."
+	text "איפה :ERIK";TODO
+	line "היא ?SARA";TODO
+	cont ".אמרה שנפגש כאן"
 	done
 
 _FuchsiaCityText4::
-	text "That item ball in"
-	line "there is really a"
-	cont "#MON."
+	text "הזה item ballה";TODO
+	line "שם הוא בעצם"
+	cont ".ימון#"
 	done
 
 _FuchsiaCityText5::
@@ -32,86 +33,87 @@ _FuchsiaCityText5::
 	done
 
 _FuchsiaCityText11::
-	text "FUCHSIA CITY"
-	line "Behold! It's"
-	cont "Passion Pink!"
+	text "FUCHSIA CITY";TODO
+	line "היזהרו! זה"
+	cont "Passion Pink!";TODO
 	done
 
 _FuchsiaCityText13::
-	text "SAFARI GAME"
-	line "#MON-U-CATCH!"
+	text "SAFARI GAME";TODO
+	line "#MON-U-CATCH!";TODO
 	done
 
 _FuchsiaCityText16::
-	text "SAFARI ZONE"
-	line "WARDEN's HOME"
+	text "SAFARI ZONE";TODO
+	line "WARDEN's HOME";TODO
 	done
 
 _FuchsiaCityText17::
-	text "#MON PARADISE"
-	line "SAFARI ZONE"
+	text "#MON PARADISE";TODO
+	line "SAFARI ZONE";TODO
 	done
 
 _FuchsiaCityText18::
-	text "FUCHSIA CITY"
-	line "#MON GYM"
-	cont "LEADER: KOGA"
+	text "FUCHSIA CITY";TODO
+	line "#MON GYM";TODO
+	cont "LEADER: KOGA";TODO
 
-	para "The Poisonous"
-	line "Ninja Master"
+	para "מאסטר הנינג'ה"
+	line ".הרעיל"
 	done
 
 _FuchsiaCityChanseyText::
-	text "Name: CHANSEY"
+	text "CHANSEY:שם";TODO
 
-	para "Catching one is"
-	line "all up to chance."
+	para "לתפוס אחד"
+	line "כזה זה עניין"
+	cont ".של מזל"
 	prompt
 
 _FuchsiaCityVoltorbText::
-	text "Name: VOLTORB"
+	text "VOLTORB:שם";TODO
 
-	para "The very image of"
-	line "a # BALL."
+	para "נראה ממש"
+	line ".כמו #כדור"
 	prompt
 
 _FuchsiaCityKangaskhanText::
-	text "Name: KANGASKHAN"
-
-	para "A maternal #MON"
-	line "that raises its"
-	cont "young in a pouch"
-	cont "on its belly."
+	text "KANGASKHAN:שם";TODO
+	
+        para "ימון אמהי#"
+	line "שמגדל את"
+	cont "צאצאיו בתוך"
+	cont ".כיס בבטן שלו"
 	prompt
 
 _FuchsiaCitySlowpokeText::
-	text "Name: SLOWPOKE"
+	text "SLOWPOKE:שם";TODO
 
-	para "Friendly and very"
-	line "slow moving."
+	para "חביב ומאוד"
+	line ".איטי"
 	prompt
 
 _FuchsiaCityLaprasText::
-	text "Name: LAPRAS"
+	text "LAPRAS:שם";TODO
 
-	para "A.K.A. the king"
-	line "of the seas."
+	para "ידוע גם"
+	line ".כמלך הימים"
 	prompt
 
 _FuchsiaCityOmanyteText::
-	text "Name: OMANYTE"
+	text "OMANYTE:שם";TODO
 
-	para "A #MON that"
-	line "was resurrected"
-	cont "from a fossil."
+	para "ימון שהפך#"
+	line ".למאובן"
+	cont ".והוחזר לחיים"
 	prompt
 
 _FuchsiaCityKabutoText::
-	text "Name: KABUTO"
+	text "KABUTO:שם"
 
-	para "A #MON that"
-	line "was resurrected"
-	cont "from a fossil."
+	para "ימון שהפך#"
+	line "למאובן"
+	cont ".והוחזר לחיים"
 	prompt
 
 _FuchsiaCityText_19b2a::
