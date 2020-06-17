ea9c9bfc06365513d8f5aa6450c3c6ce8efa4545
Shako-24 <66572118+Shako-24@users.noreply.github.com>
2020-06-17 11:58:14 +0300

    Update FuchsiaGoodRodHouse.asm

--- text/maps/FuchsiaGoodRodHouse.asm
+++ text/maps/FuchsiaGoodRodHouse.asm
@@ -1,22 +1,20 @@
 _FuchsiaHouse3Text_561bd::
-	text "אני אחיו"
-	line "הגדול של"
-	cont "!גורו הדייגים";TODO
+	text "אני אחיו הגדול של"
+	line "!גורו הדייגים"
 
 	para "אני אוההההב"
-	line "!לדוג"
+	line "לדוג!"
 
-	para "גם אתה"
-	line "?אוהב לדוג"
+	para "גם אתה אוהב לדוג?"
 	done
 
 _FuchsiaHouse3Text_561c2::
 	text "אדיר! אני"
 	line "אוהב את"
-	cont "!הסגנון שלך"
+	cont "הסגנון שלך!"
 
 	para "קח את זה"
-	line "!וצא לדוג חבוב"
+	line "וצא לדוג, חבוב!"
 
 	para "קיבל <PLAYER>"
 	line "a @"
@@ -25,20 +23,20 @@ _FuchsiaHouse3Text_561c2::
 
 _FuchsiaHouse3Text_56212::
 	text "הו... זה"
-	line "...כל כך מאכזב"
+	line "כל כך מאכזב..."
 	done
 
 _FuchsiaHouse3Text_56217::
-	text "!שלום לך"
-	line "!<PLAYER>"
+	text "שלום לך."
+	line "<PLAYER>!"
 
 	para "איך הולך"
-	line "?עם הדייג"
+	line "עם הדייג?"
 	done
 
 _FuchsiaHouse3Text_5621c::
-	text "!הו לא"
+	text "הו לא!"
 
 	para "אין לך"
-	line "!מקום למתנה שלי"
+	line "מקום למתנה שלי!"
 	done
