e2fd2d15d3679656f17317de8eeac387460a2a4a
Nog-Frog <nogatheunicorn@gmail.com>
2020-06-18 12:39:23 +0300

    Update BikeShop.asm

--- text/maps/BikeShop.asm
+++ text/maps/BikeShop.asm
@@ -20,54 +20,55 @@ _BikeShopCantAffordText::
 	prompt
 
 _BikeShopText_1d81f::
-	text "Oh, that's..."
+	text "זה..."
 
-	para "A BIKE VOUCHER!"
+	para "שובר לאופניים!"
 
-	para "OK! Here you go!"
+	para "בסדר! הנה לך!"
 	prompt
 
 _BikeShopText_1d824::
-	text "<PLAYER> exchanged"
-	line "the BIKE VOUCHER"
-	cont "for a BICYCLE.@@"
+	text "<PLAYER> החליף את"
+	line "השובר לאופניים"
+	cont "בעד זוג אופניים.@@"
 
 _BikeShopComeAgainText::
-	text "Come back again"
-	line "some time!"
+	text "נשמח לראותך"
+	line "שוב!"
 	done
 
 _BikeShopText_1d82f::
-	text "How do you like"
-	line "your new BICYCLE?"
+	text "אתה נהנה מהאופניים"
+	line "החדשים שלך?"
 
-	para "You can take it"
-	line "on CYCLING ROAD"
-	cont "and in caves!"
+	para "אתה יכול לרכב"
+	line "עליהם גם בשביל"
+	cont "האופניים או בתוך"
+	cont "מערות!"
 	done
 
 _BikeShopText_1d834::
-	text "You better make"
-	line "room for this!"
+	text "כדאי שתפנה קצת"
+	line "מקום בשביל זה!"
 	done
 
 _BikeShopText_1d843::
-	text "A plain city BIKE"
-	line "is good enough"
-	cont "for me!"
+	text "אופניים עירוניות"
+	line "מספיקות לי"
+	cont "בהחלט!"
 
-	para "You can't put a"
-	line "shopping basket"
-	cont "on an MTB!"
+	para "הרי אי אפשר להרכיב"
+	line "סל קניות על אופני"
+	cont "הרים!"
 	done
 
 _BikeShopText_1d85c::
-	text "These BIKEs are"
-	line "cool, but they're"
-	cont "way expensive!"
+	text "האופניים האלה"
+	line "מגניבים, אבל הם"
+	cont "יקרים בטירוף!"
 	done
 
 _BikeShopText_1d861::
-	text "Wow. Your BIKE is"
-	line "really cool!"
+	text "וואו. האופניים שלך"
+	line "ממש מגניבים!"
 	done
