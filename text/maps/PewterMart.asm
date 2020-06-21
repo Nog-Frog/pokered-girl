125d5dbd6bbbddc0e4a97d0bcdabec2458308196
NoamGeller <noam.geller@mail.huji.ac.il>
2020-06-21 10:52:41 +0300

    Translated PewterMart

--- text/maps/PewterMart.asm
+++ text/maps/PewterMart.asm
@@ -1,17 +1,16 @@
 _PewterMartText2::
-	text "A shady, old man"
-	line "got me to buy"
-	cont "this really weird"
-	cont "fish #MON!"
+	text "איש זקן ומפוקפק"
+	line "גרם לי לקנות את"
+	cont "את ה#ימון דג"
+	cont "המאוד מוזר הזה!"
 
-	para "It's totally weak"
-	line "and it cost ¥500!"
+	para "הוא לגמרי חלש"
+	line "והוא עלה ¥005!"
 	done
 
 _PewterMartText3::
-	text "Good things can"
-	line "happen if you"
-	cont "raise #MON"
-	cont "diligently, even"
-	cont "the weak ones!"
+	text "דברים טובים יכולים"
+	line "אם מתמידים בגידול"
+	cont "#ימון, אפילו עם"
+	cont "אלו החלשים!"
 	done
