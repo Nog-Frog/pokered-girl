099f5a94801e93d6476ae9079503c14b84cb65d4
NoamGeller <noam.geller@mail.huji.ac.il>
2020-06-21 11:10:05 +0300

    Translated PokemonMansion2F, including mew's story

--- text/maps/PokemonMansion2F.asm
+++ text/maps/PokemonMansion2F.asm
@@ -1,47 +1,47 @@
 _Mansion2BattleText1::
-	text "I can't get out!"
-	line "This old place is"
-	cont "one big puzzle!"
+	text "אני לא מצליח לצאת!"
+	line "כל המקום העתיק"
+	cont "הזה הוא פאזל אחד"
+	cont "גדול!"
 	done
 
 _Mansion2EndBattleText1::
-	text "Oh no!"
-	line "My bag of loot!"
+	text "אוי לא!"
+	line "שק השלל שלי!"
 	prompt
 
 _Mansion2AfterBattleText1::
-	text "Switches open and"
-	line "close alternating"
-	cont "sets of doors!"
+	text "מתגים פותחים"
+	line "וסוגרים סדרות"
+	cont "מתחלפות של דלתות!"
 	done
 
 _Mansion2Text3::
-	text "Diary: July 5"
-	line "Guyana,"
-	cont "South America"
+	text "יומן: 5 ליולי"
+	line "גיאנה,"
+	cont "דרום אמריקה"
 
-	para "A new #MON was"
-	line "discovered deep"
-	cont "in the jungle."
+	para "#ימון חדש התגלה"
+	line "במעמקי הג′ונגל."
 	done
 
 _Mansion2Text4::
-	text "Diary: July 10"
-	line "We christened the"
-	cont "newly discovered"
-	cont "#MON, MEW."
+	text "יומן: 10 ליולי"
+	line "נתנו ל#ימון"
+	cont "חדש שהתגלה את"
+	cont "השם תש."
 	done
 
 _Mansion2Text_520c2::
-	text "A secret switch!"
+	text "מתג חבוי!"
 
-	para "Press it?"
+	para "ללחוץ עליו?"
 	done
 
 _Mansion2Text_520c7::
-	text "Who wouldn't?"
+	text "מי לא היה לוחץ?"
 	prompt
 
 _Mansion2Text_520cc::
-	text "Not quite yet!"
+	text "עדיין לא!"
 	done
