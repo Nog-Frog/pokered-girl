b48a07b1a0ea926ae07e2c9071710b4ed89994e9
Nog-Frog <65667411+Nog-Frog@users.noreply.github.com>
2020-06-25 13:42:00 +0300

    Update Route11Gate2F.asm

--- text/maps/Route11Gate2F.asm
+++ text/maps/Route11Gate2F.asm
@@ -2,30 +2,31 @@ _Route11GateUpstairsText_494a3::
 	text "יש חפצים על האדמה"
 	line "שאי אפשר לראות."
 
-	para "מאתר חפצים יזהה"
-	line "חפץ שקרוב אליך."
+	para "מאתר חפצים יתריע"
+	line "אם יש בסביבה חפץ"
+	cont "כזה."
 
 	para "הוא לא יכול לאתר"
-	line "אותו במדויק, אז"
-	cont "תצטרך לחפש אותו"
-	cont "בעצמך!"
+	line "את המיקום המדויק"
+	cont "שלו, אז תצטרך"
+	cont "אותו בעצמך!"
 	done
 
 _BinocularsSnorlaxText::
-	text "הסתכלת במשקפת."
+	text "מביט דרך המשקפת."
 
 	para "#ימון גדול ישן"
-	line "על הדרך!"
+	line "על השביל!"
 	done
 
 _BinocularsNoSnorlaxText::
-	text "הסתכלת במשקפת."
+	text "מביט דרך המשקפת."
 
-	para "איזה מחזה מרהיב!"
+	para "הנוף מרהיב!"
 	done
 
 _Route11GateUpstairsText_494d5::
-	text "הסתכלת במשקפת."
+	text "מביט דרך המשקפת."
 
 	para "הדרך היחידה להגיע"
 	line "מעיר הטורקיז לעיר"
