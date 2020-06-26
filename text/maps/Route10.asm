5963ed4e24377418123239546db56a6f7e1d958c
Nog-Frog <nogatheunicorn@gmail.com>
2020-06-26 14:41:04 +0300

    Translate Route 10

--- text/maps/Route10.asm
+++ text/maps/Route10.asm
@@ -1,109 +1,109 @@
 _Route10BattleText1::
-	text "Wow, are you a"
-	line "#MANIAC too?"
-	cont "Want to see my"
-	cont "collection?"
+	text "וואו, גם אתה"
+	line "#יטורף? רוצה"
+	cont "לראות את האוסף"
+	cont "שלי?"
 	done
 
 _Route10EndBattleText1::
-	text "Humph."
-	line "I'm not angry!"
+	text "האמפף."
+	line "אני לא כועס!"
 	prompt
 
 _Route10AfterBattleText1::
-	text "I have more rare"
-	line "#MON at home!"
+	text "יש לי עוד"
+	line "#ימונים נדירים"
+	cont "בבית!"
 	done
 
 _Route10BattleText2::
-	text "Ha-hahah-ah-ha!"
+	text "חה-חהחה-חה-חה!"
 	done
 
 _Route10EndBattleText2::
-	text "Ha-haha!"
-	line "Not laughing!"
-	cont "Ha-hay fever!"
-	cont "Haha-ha-choo!"
+	text "חה-חהחה!"
+	line "לא צוחק!"
+	cont "קדחת הש-חה-חה-חת!"
+	cont "חה-חה-הצ'ו!"
 	prompt
 
 _Route10AfterBattleText2::
-	text "Haha-ha-choo!"
-	line "Ha-choo!"
-	cont "Snort! Snivel!"
+	text "חהחה-חה-צ'ו!"
+	line "חה-צ'ו!"
+	cont "סניף! דמעה!"
 	done
 
 _Route10BattleText3::
-	text "Hi kid, want to"
-	line "see my #MON?"
+	text "היי ילד, רוצה"
+	line "לראות את ה#ימון"
+	cont "שלי?"
 	done
 
 _Route10EndBattleText3::
-	text "Oh no!"
-	line "My #MON!"
+	text "אוי לא!"
+	line "ה#ימון שלי!"
 	prompt
 
 _Route10AfterBattleText3::
-	text "I don't like you"
-	line "for beating me!"
+	text "אני שונא אותך כי"
+	line "כיסחת אותי!"
 	done
 
 _Route10BattleText4::
-	text "I've been to a"
-	line "#MON GYM a few"
-	cont "times. But, I"
-	cont "lost each time."
+	text "כבר אתגרתי מכוני"
+	line "#ימון כמה פעמים."
+	cont "אבל הפעם, הפסדתי."
 	done
 
 _Route10EndBattleText4::
-	text "Ohh!"
-	line "Blew it again!"
+	text "אוו!"
+	line "שוב פישלתי!"
 	prompt
 
 _Route10AfterBattleText4::
-	text "I noticed some"
-	line "#MANIACs"
-	cont "prowling around."
+	text "ראיתי כמה #י-"
+	line "טורפים בסביבה"
+	cont "משחרים לטרף."
 	done
 
 _Route10BattleText5::
-	text "Ah! This mountain"
-	line "air is delicious!"
+	text "אה! אוויר ההרים"
+	line "טעים כל כך!"
 	done
 
 _Route10EndBattleText5::
-	text "That"
-	line "cleared my head!"
+	text "זה"
+	line "עזר לי לנקות את"
+	cont "הראש!"
 	prompt
 
 _Route10AfterBattleText5::
-	text "I feel bloated on"
-	line "mountain air!"
+	text "אני מרגיש נפוח"
+	line "מרוב אוויר הרים!"
 	done
 
 _Route10BattleText6::
-	text "I'm feeling a bit"
-	line "faint from this"
-	cont "tough hike."
+	text "אני מרגיש קצת שחוק"
+	line "אחרי הטיול הארוך."
 	done
 
 _Route10EndBattleText6::
-	text "I'm"
-	line "not up to it!"
+	text "אני"
+	line "לא בנוי לזה!"
 	prompt
 
 _Route10AfterBattleText6::
-	text "The #MON here"
-	line "are so chunky!"
-	cont "There should be a"
-	cont "pink one with a"
-	cont "floral pattern!"
+	text "ה#ימונים כאן כאלה"
+	line "מסורבלים! הלוואי"
+	cont "שהיה אחד ורוד עם"
+	cont "הדפס פרחוני!"
 	done
 
 _Route10Text9::
 _Route10Text7::
-	text "ROCK TUNNEL"
+	text "מנהרת הסלע"
 	done
 
 _Route10Text10::
-	text "POWER PLANT"
+	text "תחנת הכוח"
 	done
