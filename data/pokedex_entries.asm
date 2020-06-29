c41e1c17af641d3cb9e63707dadee2b98f176122
Nog-Frog <nogatheunicorn@gmail.com>
2020-06-29 11:48:29 +0300

    Translate pokemon species names

--- data/pokedex_entries.asm
+++ data/pokedex_entries.asm
@@ -196,1057 +196,1057 @@ PokedexEntryPointers:
 ; text entry
 
 RhydonDexEntry:
-	db "DRILL@"
+	db "מקדחה@"
 	db 1,90
 	dw 1202
 	TX_FAR _RhydonDexEntry
 	db "@"
 
 KangaskhanDexEntry:
-	db "PARENT@"
+	db "הורה@"
 	db 2,20
 	dw 798
 	TX_FAR _KangaskhanDexEntry
 	db "@"
 
 NidoranMDexEntry:
-	db "POISON PIN@"
+	db "חוד רעיל@"
 	db 0,50
 	dw 90
 	TX_FAR _NidoranMDexEntry
 	db "@"
 
 ClefairyDexEntry:
-	db "FAIRY@"
+	db "שדון@"
 	db 0,60
 	dw 77
 	TX_FAR _ClefairyDexEntry
 	db "@"
 
 SpearowDexEntry:
-	db "TINY BIRD@"
+	db "ציפור זעירה@"
 	db 0,30
 	dw 18
 	TX_FAR _SpearowDexEntry
 	db "@"
 
 VoltorbDexEntry:
-	db "BALL@"
+	db "כדור@"
 	db 0,50
 	dw 104
 	TX_FAR _VoltorbDexEntry
 	db "@"
 
 NidokingDexEntry:
-	db "DRILL@"
+	db "מקדחה@"
 	db 1,39
 	dw 621
 	TX_FAR _NidokingDexEntry
 	db "@"
 
 SlowbroDexEntry:
-	db "HERMITCRAB@"
+	db "סרטן נזיר@"
 	db 1,60
 	dw 784
 	TX_FAR _SlowbroDexEntry
 	db "@"
 
 IvysaurDexEntry:
-	db "SEED@"
+	db "זרע@"
 	db 0,99
 	dw 131
 	TX_FAR _IvysaurDexEntry
 	db "@"
 
 ExeggutorDexEntry:
-	db "COCONUT@"
+	db "תמר@"
 	db 2,0
 	dw 1202
 	TX_FAR _ExeggutorDexEntry
 	db "@"
 
 LickitungDexEntry:
-	db "LICKING@"
+	db "ליקוק@"
 	db 1,19
 	dw 653
 	TX_FAR _LickitungDexEntry
 	db "@"
 
 ExeggcuteDexEntry:
-	db "EGG@"
+	db "ביצה@"
 	db 0,40
 	dw 27
 	TX_FAR _ExeggcuteDexEntry
 	db "@"
 
 GrimerDexEntry:
-	db "SLUDGE@"
+	db "רפש@"
 	db 0,88
 	dw 299
 	TX_FAR _GrimerDexEntry
 	db "@"
 
 GengarDexEntry:
-	db "SHADOW@"
+	db "צל@"
 	db 1,49
 	dw 403
 	TX_FAR _GengarDexEntry
 	db "@"
 
 NidoranFDexEntry:
-	db "POISON PIN@"
+	db "חוד רעיל@"
 	db 0,40
 	dw 68
 	TX_FAR _NidoranFDexEntry
 	db "@"
 
 NidoqueenDexEntry:
-	db "DRILL@"
+	db "מקדחה@"
 	db 1,29
 	dw 598
 	TX_FAR _NidoqueenDexEntry
 	db "@"
 
 CuboneDexEntry:
-	db "LONELY@"
+	db "בודד@"
 	db 0,40
 	dw 63
 	TX_FAR _CuboneDexEntry
 	db "@"
 
 RhyhornDexEntry:
-	db "SPIKES@"
+	db "קוצים@"
 	db 0,99
 	dw 1152
 	TX_FAR _RhyhornDexEntry
 	db "@"
 
 LaprasDexEntry:
-	db "TRANSPORT@"
+	db "תחבורה@"
 	db 2,48
 	dw 2199
 	TX_FAR _LaprasDexEntry
 	db "@"
 
 ArcanineDexEntry:
-	db "LEGENDARY@"
+	db "אגדי@"
 	db 1,90
 	dw 1551
 	TX_FAR _ArcanineDexEntry
 	db "@"
 
 MewDexEntry:
-	db "NEW SPECIE@"
+	db "זן חדש@"
 	db 0,40
 	dw 40
 	TX_FAR _MewDexEntry
 	db "@"
 
 GyaradosDexEntry:
-	db "ATROCIOUS@"
+	db "אכזרי@"
 	db 6,50
 	dw 2349
 	TX_FAR _GyaradosDexEntry
 	db "@"
 
 ShellderDexEntry:
-	db "BIVALVE@"
+	db "צדפה@"
 	db 0,30
 	dw 40
 	TX_FAR _ShellderDexEntry
 	db "@"
 
 TentacoolDexEntry:
-	db "JELLYFISH@"
+	db "מדוזה@"
 	db 0,88
 	dw 453
 	TX_FAR _TentacoolDexEntry
 	db "@"
 
 GastlyDexEntry:
-	db "GAS@"
+	db "אד@"
 	db 1,29
 	dw 0
 	TX_FAR _GastlyDexEntry
 	db "@"
 
 ScytherDexEntry:
-	db "MANTIS@"
+	db "גמל שלמה@"
 	db 1,49
 	dw 557
 	TX_FAR _ScytherDexEntry
 	db "@"
 
 StaryuDexEntry:
-	db "STARSHAPE@"
+	db "כוכב@"
 	db 0,78
 	dw 344
 	TX_FAR _StaryuDexEntry
 	db "@"
 
 BlastoiseDexEntry:
-	db "SHELLFISH@"
+	db "רכיכה@"
 	db 1,60
 	dw 857
 	TX_FAR _BlastoiseDexEntry
 	db "@"
 
 PinsirDexEntry:
-	db "STAGBEETLE@"
+	db "איילית@"
 	db 1,49
 	dw 548
 	TX_FAR _PinsirDexEntry
 	db "@"
 
 TangelaDexEntry:
-	db "VINE@"
+	db "שרך@"
 	db 0,99
 	dw 349
 	TX_FAR _TangelaDexEntry
 	db "@"
 
 GrowlitheDexEntry:
-	db "PUPPY@"
+	db "כלבלב@"
 	db 0,71
 	dw 190
 	TX_FAR _GrowlitheDexEntry
 	db "@"
 
 OnixDexEntry:
-	db "ROCK SNAKE@"
+	db "נחש סלעE@"
 	db 8,78
 	dw 2100
 	TX_FAR _OnixDexEntry
 	db "@"
 
 FearowDexEntry:
-	db "BEAK@"
+	db "מקור@"
 	db 1,19
 	dw 381
 	TX_FAR _FearowDexEntry
 	db "@"
 
 PidgeyDexEntry:
-	db "TINY BIRD@"
+	db "ציפור זעירה@"
 	db 0,30
 	dw 18
 	TX_FAR _PidgeyDexEntry
 	db "@"
 
 SlowpokeDexEntry:
-	db "DOPEY@"
+	db "איטי@"
 	db 1,19
 	dw 358
 	TX_FAR _SlowpokeDexEntry
 	db "@"
 
 KadabraDexEntry:
-	db "PSI@"
+	db "על-חוש@"
 	db 1,29
 	dw 566
 	TX_FAR _KadabraDexEntry
 	db "@"
 
 GravelerDexEntry:
-	db "ROCK@"
+	db "סלע@"
 	db 0,99
 	dw 1052
 	TX_FAR _GravelerDexEntry
 	db "@"
 
 ChanseyDexEntry:
-	db "EGG@"
+	db "ביצה@"
 	db 1,9
 	dw 344
 	TX_FAR _ChanseyDexEntry
 	db "@"
 
 MachokeDexEntry:
-	db "SUPERPOWER@"
+	db "כוח על@"
 	db 1,49
 	dw 703
 	TX_FAR _MachokeDexEntry
 	db "@"
 
 MrMimeDexEntry:
-	db "BARRIER@"
+	db "מחסום@"
 	db 1,29
 	dw 544
 	TX_FAR _MrMimeDexEntry
 	db "@"
 
 HitmonleeDexEntry:
-	db "KICKING@"
+	db "בעיטה@"
 	db 1,49
 	dw 498
 	TX_FAR _HitmonleeDexEntry
 	db "@"
 
 HitmonchanDexEntry:
-	db "PUNCHING@"
+	db "איגרוף@"
 	db 1,39
 	dw 503
 	TX_FAR _HitmonchanDexEntry
 	db "@"
 
 ArbokDexEntry:
-	db "COBRA@"
+	db "פתן@"
 	db 3,50
 	dw 648
 	TX_FAR _ArbokDexEntry
 	db "@"
 
 ParasectDexEntry:
-	db "MUSHROOM@"
+	db "פטריה@"
 	db 0,99
 	dw 294
 	TX_FAR _ParasectDexEntry
 	db "@"
 
 PsyduckDexEntry:
-	db "DUCK@"
+	db "ברווז@"
 	db 0,78
 	dw 195
 	TX_FAR _PsyduckDexEntry
 	db "@"
 
 DrowzeeDexEntry:
-	db "HYPNOSIS@"
+	db "היפנוזה@"
 	db 0,99
 	dw 322
 	TX_FAR _DrowzeeDexEntry
 	db "@"
 
 GolemDexEntry:
-	db "MEGATON@"
+	db "גולם@"
 	db 1,39
 	dw 3002
 	TX_FAR _GolemDexEntry
 	db "@"
 
 MagmarDexEntry:
-	db "SPITFIRE@"
+	db "חמום מוח@"
 	db 1,29
 	dw 444
 	TX_FAR _MagmarDexEntry
 	db "@"
 
 ElectabuzzDexEntry:
-	db "ELECTRIC@"
+	db "חשמלי@"
 	db 1,9
 	dw 299
 	TX_FAR _ElectabuzzDexEntry
 	db "@"
 
 MagnetonDexEntry:
-	db "MAGNET@"
+	db "מגנטי@"
 	db 0,99
 	dw 598
 	TX_FAR _MagnetonDexEntry
 	db "@"
 
 KoffingDexEntry:
-	db "POISON GAS@"
+	db "גז רעיל@"
 	db 0,60
 	dw 9
 	TX_FAR _KoffingDexEntry
 	db "@"
 
 MankeyDexEntry:
-	db "PIG MONKEY@"
+	db "קוף חזירי@"
 	db 0,50
 	dw 281
 	TX_FAR _MankeyDexEntry
 	db "@"
 
 SeelDexEntry:
-	db "SEA LION@"
+	db "אריה ים@"
 	db 1,9
 	dw 898
 	TX_FAR _SeelDexEntry
 	db "@"
 
 DiglettDexEntry:
-	db "MOLE@"
+	db "חפרפרת@"
 	db 0,20
 	dw 9
 	TX_FAR _DiglettDexEntry
 	db "@"
 
 TaurosDexEntry:
-	db "WILD BULL@"
+	db "שור פרא@"
 	db 1,39
 	dw 884
 	TX_FAR _TaurosDexEntry
 	db "@"
 
 FarfetchdDexEntry:
-	db "WILD DUCK@"
+	db "ברווז פרא@"
 	db 0,78
 	dw 149
 	TX_FAR _FarfetchdDexEntry
 	db "@"
 
 VenonatDexEntry:
-	db "INSECT@"
+	db "חרק@"
 	db 0,99
 	dw 299
 	TX_FAR _VenonatDexEntry
 	db "@"
 
 DragoniteDexEntry:
-	db "DRAGON@"
+	db "דרקון@"
 	db 2,20
 	dw 2100
 	TX_FAR _DragoniteDexEntry
 	db "@"
 
 DoduoDexEntry:
-	db "TWIN BIRD@"
+	db "זוג שלוים@"
 	db 1,39
 	dw 390
 	TX_FAR _DoduoDexEntry
 	db "@"
 
 PoliwagDexEntry:
-	db "TADPOLE@"
+	db "ראשן@"
 	db 0,60
 	dw 122
 	TX_FAR _PoliwagDexEntry
 	db "@"
 
 JynxDexEntry:
-	db "HUMANSHAPE@"
+	db "צורת אנוש@"
 	db 1,39
 	dw 408
 	TX_FAR _JynxDexEntry
 	db "@"
 
 MoltresDexEntry:
-	db "FLAME@"
+	db "להבה@"
 	db 2,0
 	dw 598
 	TX_FAR _MoltresDexEntry
 	db "@"
 
 ArticunoDexEntry:
-	db "FREEZE@"
+	db "קיפאון@"
 	db 1,70
 	dw 553
 	TX_FAR _ArticunoDexEntry
 	db "@"
 
 ZapdosDexEntry:
-	db "ELECTRIC@"
+	db "חשמלי@"
 	db 1,60
 	dw 526
 	TX_FAR _ZapdosDexEntry
 	db "@"
 
 DittoDexEntry:
-	db "TRANSFORM@"
+	db "התמרה@"
 	db 0,30
 	dw 40
 	TX_FAR _DittoDexEntry
 	db "@"
 
 MeowthDexEntry:
-	db "SCRATCHCAT@"
+	db "חתול שורט@"
 	db 0,40
 	dw 40
 	TX_FAR _MeowthDexEntry
 	db "@"
 
 KrabbyDexEntry:
-	db "RIVER CRAB@"
+	db "סרטן מים מתוקים@"
 	db 0,40
 	dw 63
 	TX_FAR _KrabbyDexEntry
 	db "@"
 
 VulpixDexEntry:
-	db "FOX@"
+	db "שועל@"
 	db 0,60
 	dw 99
 	TX_FAR _VulpixDexEntry
 	db "@"
 
 NinetalesDexEntry:
-	db "FOX@"
+	db "שועל@"
 	db 1,9
 	dw 199
 	TX_FAR _NinetalesDexEntry
 	db "@"
 
 PikachuDexEntry:
-	db "MOUSE@"
+	db "עכבר@"
 	db 0,40
 	dw 58
 	TX_FAR _PikachuDexEntry
 	db "@"
 
 RaichuDexEntry:
-	db "MOUSE@"
+	db "עכבר@"
 	db 0,78
 	dw 299
 	TX_FAR _RaichuDexEntry
 	db "@"
 
 DratiniDexEntry:
-	db "DRAGON@"
+	db "דרקון@"
 	db 1,80
 	dw 31
 	TX_FAR _DratiniDexEntry
 	db "@"
 
 DragonairDexEntry:
-	db "DRAGON@"
+	db "דרקון@"
 	db 3,98
 	dw 163
 	TX_FAR _DragonairDexEntry
 	db "@"
 
 KabutoDexEntry:
-	db "SHELLFISH@"
+	db "רכיכה@"
 	db 0,50
 	dw 113
 	TX_FAR _KabutoDexEntry
 	db "@"
 
 KabutopsDexEntry:
-	db "SHELLFISH@"
+	db "רכיכה@"
 	db 1,29
 	dw 403
 	TX_FAR _KabutopsDexEntry
 	db "@"
 
 HorseaDexEntry:
-	db "DRAGON@"
+	db "דרקון@"
 	db 0,40
 	dw 81
 	TX_FAR _HorseaDexEntry
 	db "@"
 
 SeadraDexEntry:
-	db "DRAGON@"
+	db "דרקון@"
 	db 1,19
 	dw 249
 	TX_FAR _SeadraDexEntry
 	db "@"
 
 SandshrewDexEntry:
-	db "MOUSE@"
+	db "עכבר@"
 	db 0,60
 	dw 117
 	TX_FAR _SandshrewDexEntry
 	db "@"
 
 SandslashDexEntry:
-	db "MOUSE@"
+	db "עכבר@"
 	db 0,99
 	dw 294
 	TX_FAR _SandslashDexEntry
 	db "@"
 
 OmanyteDexEntry:
-	db "SPIRAL@"
+	db "סליל@"
 	db 0,40
 	dw 77
 	TX_FAR _OmanyteDexEntry
 	db "@"
 
 OmastarDexEntry:
-	db "SPIRAL@"
+	db "סליל@"
 	db 0,99
 	dw 349
 	TX_FAR _OmastarDexEntry
 	db "@"
 
 JigglypuffDexEntry:
-	db "BALLOON@"
+	db "בלון@"
 	db 0,50
 	dw 54
 	TX_FAR _JigglypuffDexEntry
 	db "@"
 
 WigglytuffDexEntry:
-	db "BALLOON@"
+	db "בלון@"
 	db 0,99
 	dw 117
 	TX_FAR _WigglytuffDexEntry
 	db "@"
 
 EeveeDexEntry:
-	db "EVOLUTION@"
+	db "פיתוח@"
 	db 0,30
 	dw 63
 	TX_FAR _EeveeDexEntry
 	db "@"
 
 FlareonDexEntry:
-	db "FLAME@"
+	db "להבה@"
 	db 0,88
 	dw 249
 	TX_FAR _FlareonDexEntry
 	db "@"
 
 JolteonDexEntry:
-	db "LIGHTNING@"
+	db "ברק@"
 	db 0,78
 	dw 244
 	TX_FAR _JolteonDexEntry
 	db "@"
 
 VaporeonDexEntry:
-	db "BUBBLE JET@"
+	db "סילון בועות@"
 	db 0,99
 	dw 290
 	TX_FAR _VaporeonDexEntry
 	db "@"
 
 MachopDexEntry:
-	db "SUPERPOWER@"
+	db "כוח על@"
 	db 0,78
 	dw 195
 	TX_FAR _MachopDexEntry
 	db "@"
 
 ZubatDexEntry:
-	db "BAT@"
+	db "עטלף@"
 	db 0,78
 	dw 77
 	TX_FAR _ZubatDexEntry
 	db "@"
 
 EkansDexEntry:
-	db "SNAKE@"
+	db "נחש@"
 	db 2,0
 	dw 68
 	TX_FAR _EkansDexEntry
 	db "@"
 
 ParasDexEntry:
-	db "MUSHROOM@"
+	db "פטריה@"
 	db 0,30
 	dw 54
 	TX_FAR _ParasDexEntry
 	db "@"
 
 PoliwhirlDexEntry:
-	db "TADPOLE@"
+	db "ראשן@"
 	db 0,99
 	dw 199
 	TX_FAR _PoliwhirlDexEntry
 	db "@"
 
 PoliwrathDexEntry:
-	db "TADPOLE@"
+	db "ראשן@"
 	db 1,29
 	dw 539
 	TX_FAR _PoliwrathDexEntry
 	db "@"
 
 WeedleDexEntry:
-	db "HAIRY BUG@"
+	db "זחל דובון@"
 	db 0,30
 	dw 31
 	TX_FAR _WeedleDexEntry
 	db "@"
 
 KakunaDexEntry:
-	db "COCOON@"
+	db "גולם@"
 	db 0,60
 	dw 99
 	TX_FAR _KakunaDexEntry
 	db "@"
 
 BeedrillDexEntry:
-	db "POISON BEE@"
+	db "דבור ארסי@"
 	db 0,99
 	dw 294
 	TX_FAR _BeedrillDexEntry
 	db "@"
 
 DodrioDexEntry:
-	db "TRIPLEBIRD@"
+	db "שלישית שליו@"
 	db 1,80
 	dw 852
 	TX_FAR _DodrioDexEntry
 	db "@"
 
 PrimeapeDexEntry:
-	db "PIG MONKEY@"
+	db "קוף חזירי@"
 	db 0,99
 	dw 322
 	TX_FAR _PrimeapeDexEntry
 	db "@"
 
 DugtrioDexEntry:
-	db "MOLE@"
+	db "חפרפרת@"
 	db 0,71
 	dw 331
 	TX_FAR _DugtrioDexEntry
 	db "@"
 
 VenomothDexEntry:
-	db "POISONMOTH@"
+	db "עש ארסי@"
 	db 1,49
 	dw 127
 	TX_FAR _VenomothDexEntry
 	db "@"
 
 DewgongDexEntry:
-	db "SEA LION@"
+	db "אריה ים@"
 	db 1,70
 	dw 1202
 	TX_FAR _DewgongDexEntry
 	db "@"
 
 CaterpieDexEntry:
-	db "WORM@"
+	db "זחל@"
 	db 0,30
 	dw 27
 	TX_FAR _CaterpieDexEntry
 	db "@"
 
 MetapodDexEntry:
-	db "COCOON@"
+	db "גולם@"
 	db 0,71
 	dw 99
 	TX_FAR _MetapodDexEntry
 	db "@"
 
 ButterfreeDexEntry:
-	db "BUTTERFLY@"
+	db "פרפר@"
 	db 1,9
 	dw 322
 	TX_FAR _ButterfreeDexEntry
 	db "@"
 
 MachampDexEntry:
-	db "SUPERPOWER@"
+	db "כוח על@"
 	db 1,60
 	dw 1301
 	TX_FAR _MachampDexEntry
 	db "@"
 
 GolduckDexEntry:
-	db "DUCK@"
+	db "ברווז@"
 	db 1,70
 	dw 766
 	TX_FAR _GolduckDexEntry
 	db "@"
 
 HypnoDexEntry:
-	db "HYPNOSIS@"
+	db "היפנוזה@"
 	db 1,60
 	dw 757
 	TX_FAR _HypnoDexEntry
 	db "@"
 
 GolbatDexEntry:
-	db "BAT@"
+	db "עטלף@"
 	db 1,60
 	dw 548
 	TX_FAR _GolbatDexEntry
 	db "@"
 
 MewtwoDexEntry:
-	db "GENETIC@"
+	db "גנים@"
 	db 2,0
 	dw 1220
 	TX_FAR _MewtwoDexEntry
 	db "@"
 
 SnorlaxDexEntry:
-	db "SLEEPING@"
+	db "ישנוני@"
 	db 2,10
 	dw 4599
 	TX_FAR _SnorlaxDexEntry
 	db "@"
 
 MagikarpDexEntry:
-	db "FISH@"
+	db "דג@"
 	db 0,88
 	dw 99
 	TX_FAR _MagikarpDexEntry
 	db "@"
 
 MukDexEntry:
-	db "SLUDGE@"
+	db "רפש@"
 	db 1,19
 	dw 299
 	TX_FAR _MukDexEntry
 	db "@"
 
 KinglerDexEntry:
-	db "PINCER@"
+	db "צבת@"
 	db 1,29
 	dw 598
 	TX_FAR _KinglerDexEntry
 	db "@"
 
 CloysterDexEntry:
-	db "BIVALVE@"
+	db "צדפה@"
 	db 1,49
 	dw 1324
 	TX_FAR _CloysterDexEntry
 	db "@"
 
 ElectrodeDexEntry:
-	db "BALL@"
+	db "כדור@"
 	db 1,19
 	dw 666
 	TX_FAR _ElectrodeDexEntry
 	db "@"
 
 ClefableDexEntry:
-	db "FAIRY@"
+	db "שדון@"
 	db 1,29
 	dw 399
 	TX_FAR _ClefableDexEntry
 	db "@"
 
 WeezingDexEntry:
-	db "POISON GAS@"
+	db "גז רעיל@"
 	db 1,19
 	dw 95
 	TX_FAR _WeezingDexEntry
 	db "@"
 
 PersianDexEntry:
-	db "CLASSY CAT@"
+	db "חתול סיאמי@"
 	db 0,99
 	dw 322
 	TX_FAR _PersianDexEntry
 	db "@"
 
 MarowakDexEntry:
-	db "BONEKEEPER@"
+	db "אספן עצמות@"
 	db 0,99
 	dw 449
 	TX_FAR _MarowakDexEntry
 	db "@"
 
 HaunterDexEntry:
-	db "GAS@"
+	db "אד@"
 	db 1,60
 	dw 0
 	TX_FAR _HaunterDexEntry
 	db "@"
 
 AbraDexEntry:
-	db "PSI@"
+	db "על-חוש@"
 	db 0,88
 	dw 195
 	TX_FAR _AbraDexEntry
 	db "@"
 
 AlakazamDexEntry:
-	db "PSI@"
+	db "על-חוש@"
 	db 1,49
 	dw 480
 	TX_FAR _AlakazamDexEntry
 	db "@"
 
 PidgeottoDexEntry:
-	db "BIRD@"
+	db "ציפור@"
 	db 1,9
 	dw 299
 	TX_FAR _PidgeottoDexEntry
 	db "@"
 
 PidgeotDexEntry:
-	db "BIRD@"
+	db "ציפור@"
 	db 1,49
 	dw 394
 	TX_FAR _PidgeotDexEntry
 	db "@"
 
 StarmieDexEntry:
-	db "MYSTERIOUS@"
+	db "מסתורין@"
 	db 1,9
 	dw 798
 	TX_FAR _StarmieDexEntry
 	db "@"
 
 BulbasaurDexEntry:
-	db "SEED@"
+	db "זרע@"
 	db 0,71
 	dw 68
 	TX_FAR _BulbasaurDexEntry
 	db "@"
 
 VenusaurDexEntry:
-	db "SEED@"
+	db "זרע@"
 	db 2,0
 	dw 1002
 	TX_FAR _VenusaurDexEntry
 	db "@"
 
 TentacruelDexEntry:
-	db "JELLYFISH@"
+	db "מדוזה@"
 	db 1,60
 	dw 548
 	TX_FAR _TentacruelDexEntry
 	db "@"
 
 GoldeenDexEntry:
-	db "GOLDFISH@"
+	db "דג זהב@"
 	db 0,60
 	dw 149
 	TX_FAR _GoldeenDexEntry
 	db "@"
 
 SeakingDexEntry:
-	db "GOLDFISH@"
+	db "דג זהב@"
 	db 1,29
 	dw 390
 	TX_FAR _SeakingDexEntry
 	db "@"
 
 PonytaDexEntry:
-	db "FIRE HORSE@"
+	db "סוס אש@"
 	db 0,99
 	dw 299
 	TX_FAR _PonytaDexEntry
 	db "@"
 
 RapidashDexEntry:
-	db "FIRE HORSE@"
+	db "סוס אש@"
 	db 1,70
 	dw 948
 	TX_FAR _RapidashDexEntry
 	db "@"
 
 RattataDexEntry:
-	db "RAT@"
+	db "עכברוש@"
 	db 0,30
 	dw 36
 	TX_FAR _RattataDexEntry
 	db "@"
 
 RaticateDexEntry:
-	db "RAT@"
+	db "עכברוש@"
 	db 0,71
 	dw 185
 	TX_FAR _RaticateDexEntry
 	db "@"
 
 NidorinoDexEntry:
-	db "POISON PIN@"
+	db "חוד רעיל@"
 	db 0,88
 	dw 195
 	TX_FAR _NidorinoDexEntry
 	db "@"
 
 NidorinaDexEntry:
-	db "POISON PIN@"
+	db "חוד רעיל@"
 	db 0,78
 	dw 199
 	TX_FAR _NidorinaDexEntry
 	db "@"
 
 GeodudeDexEntry:
-	db "ROCK@"
+	db "סלע@"
 	db 0,40
 	dw 199
 	TX_FAR _GeodudeDexEntry
 	db "@"
 
 PorygonDexEntry:
-	db "VIRTUAL@"
+	db "מדומה@"
 	db 0,78
 	dw 362
 	TX_FAR _PorygonDexEntry
 	db "@"
 
 AerodactylDexEntry:
-	db "FOSSIL@"
+	db "מאובן@"
 	db 1,80
 	dw 589
 	TX_FAR _AerodactylDexEntry
 	db "@"
 
 MagnemiteDexEntry:
-	db "MAGNET@"
+	db "מגנט@"
 	db 0,30
 	dw 58
 	TX_FAR _MagnemiteDexEntry
 	db "@"
 
 CharmanderDexEntry:
-	db "LIZARD@"
+	db "לטאה@"
 	db 0,60
 	dw 86
 	TX_FAR _CharmanderDexEntry
 	db "@"
 
 SquirtleDexEntry:
-	db "TINYTURTLE@"
+	db "צב קטן@"
 	db 0,50
 	dw 90
 	TX_FAR _SquirtleDexEntry
 	db "@"
 
 CharmeleonDexEntry:
-	db "FLAME@"
+	db "להבה@"
 	db 1,9
 	dw 190
 	TX_FAR _CharmeleonDexEntry
 	db "@"
 
 WartortleDexEntry:
-	db "TURTLE@"
+	db "צב@"
 	db 0,99
 	dw 226
 	TX_FAR _WartortleDexEntry
 	db "@"
 
 CharizardDexEntry:
-	db "FLAME@"
+	db "להבה@"
 	db 1,70
 	dw 907
 	TX_FAR _CharizardDexEntry
 	db "@"
 
 OddishDexEntry:
-	db "WEED@"
+	db "עשב@"
 	db 0,50
 	dw 54
 	TX_FAR _OddishDexEntry
 	db "@"
 
 GloomDexEntry:
-	db "WEED@"
+	db "עשב@"
 	db 0,78
 	dw 86
 	TX_FAR _GloomDexEntry
 	db "@"
 
 VileplumeDexEntry:
-	db "FLOWER@"
+	db "פרח@"
 	db 1,19
 	dw 185
 	TX_FAR _VileplumeDexEntry
 	db "@"
 
 BellsproutDexEntry:
-	db "FLOWER@"
+	db "פרח@"
 	db 0,71
 	dw 40
 	TX_FAR _BellsproutDexEntry
 	db "@"
 
 WeepinbellDexEntry:
-	db "FLYCATCHER@"
+	db "דיונאה@"
 	db 0,99
 	dw 63
 	TX_FAR _WeepinbellDexEntry
 	db "@"
 
 VictreebelDexEntry:
-	db "FLYCATCHER@"
+	db "דיונאה@"
 	db 1,70
 	dw 154
 	TX_FAR _VictreebelDexEntry
