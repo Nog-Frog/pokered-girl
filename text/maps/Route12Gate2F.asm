46b6e7754d4a7c1217a0e8e79735111649d3b3a6
Nog-Frog <nogatheunicorn@gmail.com>
2020-06-30 16:10:35 +0300

    Translate

--- text/maps/Route12Gate2F.asm
+++ text/maps/Route12Gate2F.asm
@@ -1,42 +1,39 @@
 _TM39PreReceiveText::
-	text "My #MON's"
-	line "ashes are stored"
-	cont "in #MON TOWER."
+	text "האפר של ה#ימון"
+	line "שלי נמצא במגדל"
+	cont "ה#ימון."
 
-	para "You can have this"
-	line "TM. I don't need"
-	cont "it any more..."
+	para "קח, אני לא צריך את"
+	line "זה יותר..."
 	prompt
 
 _ReceivedTM39Text::
-	text "<PLAYER> received"
-	line "TM39!@@"
+	text "<PLAYER> קיבל"
+	line "93MT!@@"
 
 _TM39ExplanationText::
-	text "TM39 is a move"
-	line "called SWIFT."
+	text "93MT מכיל מהלך"
+	line "בשם חיש קל."
 
-	para "It's very accurate,"
-	line "so use it during"
-	cont "battles you can't"
-	cont "afford to lose."
+	para "הוא מדויק במיוחד,"
+	line "אז כדאי להשתמש בו"
+	cont "בקרבות שפשוט אסור"
+	cont "להפסיד בהם."
 	done
 
 _TM39NoRoomText::
-	text "You don't have"
-	line "room for this."
+	text "אין לך מקום לסחוב"
+	line "את זה."
 	done
 
 _Route12GateUpstairsText_495b8::
-	text "Looked into the"
-	line "binoculars."
+	text "מביט דרך המשקפת."
 
-	para "A man fishing!"
+	para "יש שם דייג!"
 	done
 
 _Route12GateUpstairsText_495c4::
-	text "Looked into the"
-	line "binoculars."
+	text "מביט דרך המשקפת."
 
-	para "It's #MON TOWER!"
+	para "זה מגדל ה#ימון!"
 	done
