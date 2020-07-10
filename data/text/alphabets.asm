112e5c37f45195fdc6104c4259d9b38e81a826e1
Nog-Frog <nogatheunicorn@gmail.com>
2020-07-10 15:29:15 +0300

    Modify naming screen keyboard

--- data/text/alphabets.asm
+++ data/text/alphabets.asm
@@ -4,12 +4,12 @@ HebrewAlphabet:
 	db "עףפץצקרשת"
 	db " ×():][<MN><PK>"
 	db "<DOT>-?!¥♂♀/<ED>"
-	db "אנגלית@"
+	db "HSILGNE@"
 
 EnglishAlphabet:
 	db "IHGFEDCBA"
 	db "RQPONMLKJ"
-	db "ZYXWVUTS "
-	db ";×():][<MN><PK>"
+	db ";ZYXWVUTS"
+	db " ×():][<MN><PK>"
 	db "<DOT>-?!¥♂♀/<ED>"
-	db "עברית @"
+	db "עברית  @"
